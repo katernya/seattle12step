0c74b584d7691aef608a96aaaa4bd7b8b31817c6
Katernya Lilia <katernya@gmail.com>
2012-02-08 21:34:45 -0700

    Variety of updates

--- aa/parse.pl
+++ aa/parse.pl
@@ -1,8 +1,37 @@
+require RecoveryAlphabet::Geo::Places;
+
+%cities = (#'Seattle' => 1,
+	   'Mtlk Terr' => 'Mountlake Terrace',
+	   'Lk Forest Pk' => 'Lake Forest Park',
+	   'Lynwd' => 'Lynnwood',
+	   #'Shoreline', 'Edmonds', 'Lynnwood', 'Mountlake Terrace', 'SeaTac', 'Vashon', 'Burien', 'Renton', 'Kent', 'Auburn', 'Maple Valley', 'Enumclaw', 'Federal Way', 'Covington',
+	   'B\'vue' => 'Bellevue',
+	   #'Bothell', 'Kirkland', 'Issaquah', 'Snoqualmie', 'Redmond', 'Duvall',
+	   'N Bend' => 'North Bend',
+	   #'Hunts Point', 'Mercer Island', 'Preston', 'Clyde Hill', 'Tukwila', 'Black Diamond',
+	   'Snoq' => 'Snoqualmie',
+# 'Bellevue',
+	   'Wdnville' => 'Woodinville',
+	   'Samm\\.' => 'Sammamish',
+# 'Fall City', 'Kenmore', 'Medina', 'Mukilteo',
+	   'Blk Dia' => 'Black Diamond',
+#'Tacoma',
+	   'Fed Wy' => 'Federal Way',
+	   'DesMoines' => 'Des Moines',
+#, 'Snohomish'
+	   'Mtlke Terrace' => 'Mountlake Terrace',
+	   #, 'Riverton', 'Hobart', 'Sammamish', 'Woodinville', 'Carnation', 'Morgan', 'North Bend',
+	   'Blk Dia\\.' => 'Black Diamond',
+#, 'Snoqualmie Pass'
+);
 
 sub parseaddress {
     my $add = shift @_;
     $add =~ s!\(.*\)!!s;
+    my $rgxp = join("|",keys %::cities);
+$add =~ s/($rgxp)/$::cities{$1}/ge;
     my $zip;
+    print STDERR $add, "\n";
     if($add =~ s!(\D{3})(98\d\d\d)!\1!)
       {
 	$zip = $2;
